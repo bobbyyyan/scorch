6807e419f56a60b8384d6354d5b5a59c18232e96
Bobby Yan <me@bobbyy.org>
2023-01-14 11:12:54 -0800

    housekeeping

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,1 +1,17 @@
-black .
+# Check Python version is at least 3.9
+PYTHON_VERSION_MIN_MET=$(python3 -c 'import sys; print(sys.version_info[:2] >= (3, 9))')
+if [ "$PYTHON_VERSION_MIN_MET" == "False" ]; then
+  echo "Python version must be >=3.9, but is $(python3 --version 2>&1)"
+  exit 1
+fi
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Pin dependencies
+pip freeze -r requirements.txt > requirements.lock
+
+# Code style and linting
+black --check --diff .
+mypy --install-types --non-interactive --show-error-codes --show-column-numbers --pretty .
+flake8 .
