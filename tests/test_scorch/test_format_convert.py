35d9778c099570bc36e1ad88fb34ce5bd2f33937
Bobby Yan <me@bobbyy.org>
2023-04-06 10:01:17 -0700

    coo to dense

--- tests/test_scorch/test_format_convert.py
+++ tests/test_scorch/test_format_convert.py
@@ -35,3 +35,19 @@ def test_2d_ds_oo():
         value=torch.tensor([1, 2, 3, 4, 0, 5]),
     )
     matrix.to_sparse(get_format_from_list(["d", "s"]))
+
+
+def test_2d_dd_oo():
+    # Test converting a COO matrix to a dense matrix
+    matrix = Tensor(
+        shape=(5, 5),
+        index=TensorIndex(
+            tensor_format=get_format_from_list(["o", "o"]),
+            mode_indices=[
+                [torch.tensor([0, 1, 2, 2, 3, 4])],
+                [torch.tensor([0, 1, 2, 3, 3, 4])],
+            ],
+        ),
+        value=torch.tensor([1, 2, 3, 4, 0, 5]),
+    )
+    matrix.to_sparse(get_format_from_list(["d", "d"]))
