bbcfa21df155ac79ddd4090f704d3151bdb311ce
Bobby Yan <me@bobbyy.org>
2023-04-26 12:34:46 -0700

    result tensor shortname

--- src/scorch/ops.py
+++ src/scorch/ops.py
@@ -93,7 +93,10 @@ def einsum(
         output_format = TensorFormat(level_formats)
 
     # Create the result TensorVar
-    result_tensor_var = TensorVar(name="RESULT", fmt=output_format)
+    available_names = [
+        x for x in list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") if x not in tensor_names
+    ]
+    result_tensor_var = TensorVar(name=available_names[0], fmt=output_format)
 
     # Generate the python code for constructing the TensorAccess's, and TensorAssign and execute it
     assert index_var_dict, "index_var_dict is empty"
