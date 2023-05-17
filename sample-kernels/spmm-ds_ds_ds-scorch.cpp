c598e920954f8d428404168f2ebbd8f45335b79d
Bobby Yan <me@bobbyy.org>
2023-05-17 16:52:53 -0700

    update sample spmm w/ wksp

--- sample-kernels/spmm-ds_ds_ds-scorch.cpp
+++ sample-kernels/spmm-ds_ds_ds-scorch.cpp
@@ -4,66 +4,73 @@ int cmp(const void* a, const void* b) {
   return *((const int*)a) - *((const int*)b);
 }
 
-int evaluate(taco_tensor_t* A, taco_tensor_t* B, taco_tensor_t* C) {
-  int A0_size = (int)(A->dimensions[0]);
-  int* A1_pos = (int*)(A->indices[1][0]);
-  int* A1_crd = (int*)(A->indices[1][1]);
-  double* A_vals = (double*)(A->vals);
-  int B0_size = (int)(B->dimensions[0]);
-  int* B1_pos = (int*)(B->indices[1][0]);
-  int* B1_crd = (int*)(B->indices[1][1]);
-  double* B_vals = (double*)(B->vals);
-  int C0_size = (int)(C->dimensions[0]);
-  int C1_size = (int)(C->dimensions[1]);
-  int* C1_pos = (int*)(C->indices[1][0]);
-  int* C1_crd = (int*)(C->indices[1][1]);
-  double* C_vals = (double*)(C->vals);
+Tensor evaluate(std::vector<int> result_shape, std::vector<int> B_shape,
+                std::vector<std::vector<torch::Tensor>> B_mode_indices,
+                torch::Tensor B_values, std::vector<int> C_shape,
+                std::vector<std::vector<torch::Tensor>> C_mode_indices,
+                torch::Tensor C_values) {
+  // Init result tensor level sizes
+  int A0_size = result_shape[0];
+
+  // Get tensor level arrays
+  int B0_size = B_shape[0];
+  torch::Tensor B1_pos = B_mode_indices[1][0];
+  torch::Tensor B1_crd = B_mode_indices[1][1];
+  int C0_size = C_shape[0];
+  torch::Tensor C1_pos = C_mode_indices[1][0];
+  torch::Tensor C1_crd = C_mode_indices[1][1];
 
   cvector<int> A1_pos = cvector<int>(A0_size + 1);
   A1_pos[0] = 0;
-  for (int32_t pA1 = 1; pA1 < (A0_size + 1); pA1++) {
+  for (int pA1 = 1; pA1 < (A0_size + 1); pA1++) {
     A1_pos[pA1] = 0;
   }
 
   cvector<int> A1_crd;
-  int32_t pA1 = 0;
+  int pA1 = 0;
 
   cvector<double> A_vals;
 
-  coo_workspace<double> wksp = coo_workspace<double>(1);
+  for (int i = 0; i < B0_size; i++) {
+    int pA0 = i;
 
-  for (int32_t i = 0; i < B0_size; i++) {
-    for (int32_t pB1 = B1_pos[i]; pB1 < B1_pos[i + 1]; pB1++) {
-      int32_t k = B1_crd[pB1];
-      for (int32_t pC1 = C1_pos[k]; pC1 < C1_pos[k + 1]; pC1++) {
-        int32_t j = C1_crd[pC1];
+    coo_workspace<double> wksp = coo_workspace<double>(1);
+
+    for (int pB1 = B1_pos[i]; pB1 < B1_pos[i + 1]; pB1++) {
+      int k = B1_crd[pB1];
+      for (int pC1 = C1_pos[k]; pC1 < C1_pos[k + 1]; pC1++) {
+        int j = C1_crd[pC1];
 
         wksp.insert({j}, B_vals[pB1] * C_vals[pC1]);
       }
     }
 
-    int32_t pA1_begin = pA1;
+    int pA1_begin = pA1;
 
     auto wksp_map = wksp.get_map();
+
     for (auto it = wksp_map.begin(); it != wksp_map.end(); ++it) {
-      int32_t j = it->first[0];
+      int j = it->first[0];
       double w_val = it->second;
 
       A_vals[pA1] = w_val;
       A1_crd[pA1] = j;
       pA1++;
     }
-    A1_pos[i + 1] = pA1 - pA1_begin;
+
+    A1_pos.push_back(A1_crd.size());
   }
 
-  int32_t csA1 = 0;
-  for (int32_t pA10 = 1; pA10 < (A0_size + 1); pA10++) {
-    csA1 += A1_pos[pA10];
-    A1_pos[pA10] = csA1;
-  }
-
-  A->indices[1][0] = (uint8_t*)(A1_pos);
-  A->indices[1][1] = (uint8_t*)(A1_crd);
-  A->vals = (uint8_t*)A_vals;
-  return 0;
+  // Assemble result
+  Tensor A;
+  torch::Tensor A1_pos_torch = torch::from_blob(
+      A1_pos.data(), {A1_pos.size()}, A1_pos.get_deleter(), torch::kInt);
+  torch::Tensor A1_crd_torch = torch::from_blob(
+      A1_crd.data(), {A1_crd.size()}, A1_crd.get_deleter(), torch::kInt);
+  torch::Tensor A_values_torch =
+      torch::from_blob(A_values.data(), {A_values.size()},
+                       A_values.get_deleter(), torch::kFloat32);
+  A._storage._index.mode_indices = {{}, {A1_pos_torch, A1_crd_torch}};
+  A._storage._value = A_values_torch;
+  return A;
 }
