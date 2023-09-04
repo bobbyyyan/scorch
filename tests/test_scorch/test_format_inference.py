658b8eb408a9c8a5fe0ad8749ded1494a6d3a6fb
Bobby Yan <me@bobbyy.org>
2023-09-04 10:24:56 -0700

    fmt inf tests

---
+++ tests/test_scorch/test_format_inference.py
@@ -0,0 +1,33 @@
+import torch
+
+from scorch import Tensor, einsum
+
+
+def test_spmm_oo_dd_fmtinf():
+    # random 5x5 torch tensors
+    tensor_a_torch = torch.rand(5, 5)
+    tensor_b_torch = torch.rand(5, 5)
+
+    a_scorch = Tensor.from_torch(tensor_a_torch, "A").to_sparse("oo")
+    b_scorch = Tensor.from_torch(tensor_b_torch, "B").to_dense()
+
+    result = einsum("ik,kj->ij", a_scorch, b_scorch)
+
+    result_torch = torch.matmul(tensor_a_torch, tensor_b_torch)
+
+    assert torch.allclose(result.to_torch(), result_torch)
+
+
+def test_spmm_ds_dd_fmtinf():
+    # random 5x5 torch tensors
+    tensor_a_torch = torch.rand(5, 5)
+    tensor_b_torch = torch.rand(5, 5)
+
+    a_scorch = Tensor.from_torch(tensor_a_torch, "A").to_sparse("ds")
+    b_scorch = Tensor.from_torch(tensor_b_torch, "B").to_dense()
+
+    result = einsum("ik,kj->ij", a_scorch, b_scorch)
+
+    result_torch = torch.matmul(tensor_a_torch, tensor_b_torch)
+
+    assert torch.allclose(result.to_torch(), result_torch)
