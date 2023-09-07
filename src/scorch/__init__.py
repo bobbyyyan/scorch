253886ba184d80499d3bb53fccafb745cecde6aa
Bobby Yan <me@bobbyy.org>
2023-09-07 11:58:21 -0700

    alias

--- src/scorch/__init__.py
+++ src/scorch/__init__.py
@@ -2,6 +2,9 @@ from .ops import einsum, matmul, matmul_wksp, precompile_kernels
 from .tensor import Tensor
 from .format import TensorFormat
 
+from_torch = Tensor.from_torch
+from_coo = Tensor.from_coo
+
 precompile_kernels()
 
 __version__ = "0.0.1"
@@ -10,6 +13,8 @@ __all__ = [
     "Tensor",
     "TensorFormat",
     "einsum",
+    "from_torch",
+    "from_coo",
     "matmul",
     "matmul_wksp",
     "__version__",
