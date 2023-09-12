0af00bf0870b3640b630273056a9a87c21a7f7f5
Bobby Yan <me@bobbyy.org>
2023-09-12 16:05:39 -0700

    spmm benchmark

--- examples/gcn/spmm.py
+++ examples/gcn/spmm.py
@@ -1,8 +1,9 @@
-# Benchmark runtime of Scorch
-
+import gc
 import torch
 import time
 import scorch
+import multiprocessing
+from statistics import mean, stdev
 
 
 def scorch_spmm(A, B):
@@ -20,17 +21,28 @@ def gen_rand_sparse_coo(dim_m, dim_n, sparsity):
     sparsity is the percentage of zero elements
     """
     nnz = int((1 - sparsity) * dim_m * dim_n)
-    indices = torch.randint(0, dim_m, size=(2, nnz))
-    values = torch.rand(nnz)
+    all_indices = torch.cartesian_prod(torch.arange(dim_m), torch.arange(dim_n))
+    perm = torch.randperm(all_indices.shape[0])
+    indices = all_indices[perm][:nnz].T
 
-    _, inverse_indices = torch.unique(indices, dim=1, return_inverse=True)
-    while len(inverse_indices) != len(indices.transpose(0, 1)):
-        indices = torch.randint(0, dim_m, size=(2, nnz))
-        _, inverse_indices = torch.unique(indices, dim=1, return_inverse=True)
+    # Indices are already unique, so no need to check for uniqueness again.
+
+    # Create a tensor that concatenates the indices along the second dimension.
+    # Then, sort this tensor along the second dimension.
+    # This will effectively sort the indices by row first and then by column.
+    indices_flattened = indices[0] * dim_n + indices[1]
+    sorted_indices = torch.argsort(indices_flattened)
+
+    # Use the sorted indices to reorder the original indices and values.
+    indices = indices[:, sorted_indices]
+    # Add a small constant to ensure all values are non-zero
+    values = torch.rand(nnz)
+    values = values[sorted_indices]
 
     A = scorch.from_coo(indices, values, (dim_m, dim_n))
+    A_torch = torch.sparse_coo_tensor(indices, values, (dim_m, dim_n)).coalesce()
 
-    return A
+    return A, A_torch
 
 
 def gen_rand_sparse_csr(dim_m, dim_n, sparsity):
@@ -40,40 +52,50 @@ def gen_rand_sparse_csr(dim_m, dim_n, sparsity):
 
 
 # Create sparse matrix
-dim_m = 5000
+dim_m = 10000
 dim_n = dim_m
 dim_k = dim_m
 
-if __name__ == "__main__":
+
+def main():
     scorch_times = []
     torch_times = []
 
-    # Warm up
-    # for _ in range(5):
-    #     scorch_spmm(A, B)
-    #     torch_spmm(A_torch, B)
+    for _ in range(3):
+        A, A_torch = gen_rand_sparse_coo(dim_m, dim_n, 0.99)
 
-    for _ in range(5):
-        # A = gen_rand_sparse_coo(dim_m, dim_n, 0.99)
-        # A_torch = A.to_torch(in_place=False).to_sparse_coo()
+        assert torch.allclose(A.values, A_torch.values().flatten())
 
-        A = gen_rand_sparse_csr(dim_m, dim_n, 0.99)
-        A_torch = A.to_torch(in_place=False)
-        A_torch = A_torch.to_sparse_csr()
+        # A = gen_rand_sparse_csr(dim_m, dim_n, 0.99)
+        # A_torch = A.to_torch(in_place=False)
+        # A_torch = A_torch.to_sparse_csr()
 
-        B = torch.rand(dim_n, dim_k)
+        B_torch = torch.rand(dim_n, dim_k)
+        B = scorch.from_torch(B_torch)
+
+        # Warm up
+        for _ in range(1):
+            scorch_spmm(A, B)
+
+        # gc.collect()
 
         start = time.perf_counter()
         C = scorch_spmm(A, B)
         end = time.perf_counter()
         scorch_times.append(end - start)
 
+        # Warm up
+        for _ in range(1):
+            torch_spmm(A_torch, B_torch)
+
+        # gc.collect()
+
         start = time.perf_counter()
-        C = torch_spmm(A_torch, B)
+        C_torch = torch_spmm(A_torch, B_torch)
         end = time.perf_counter()
         torch_times.append(end - start)
 
-        # assert torch.allclose(C.to_torch(), torch.matmul(A_torch, B))
+        assert torch.allclose(C.to_torch(), C_torch)
 
     print("dim_m: ", dim_m)
     print("A's nnz count: ", A._nnz())
@@ -81,7 +103,21 @@ if __name__ == "__main__":
 
     print("Scorch times: \n", scorch_times)
     print("PyTorch times: \n", torch_times)
+    print("Average Scorch time: ", mean(scorch_times))
+    print("Average PyTorch time: ", mean(torch_times))
+    print("std Scorch time: ", stdev(scorch_times))
+    print("std PyTorch time: ", stdev(torch_times))
+    print("std/mean Scorch: ", stdev(scorch_times) / mean(scorch_times))
+    print("std/mean PyTorch: ", stdev(torch_times) / mean(torch_times))
     print(
         "PyTorch/Scorch: \n",
         [torch_times[i] / scorch_times[i] for i in range(len(scorch_times))],
     )
+    print("Average PyTorch/Scorch: ", sum(torch_times) / sum(scorch_times))
+
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method('spawn')  # Set start method to 'spawn'
+    p = multiprocessing.Process(target=main)  # Create a Process object
+    p.start()  # Start the process
+    p.join()  # Wait for the process to complete
