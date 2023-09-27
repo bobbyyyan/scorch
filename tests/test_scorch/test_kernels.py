a4dc2da8943708be76ed2654a2614c04db8cecb2
Bobby Yan <me@bobbyy.org>
2023-09-26 19:02:15 -0700

    tests

--- tests/test_scorch/test_kernels.py
+++ tests/test_scorch/test_kernels.py
@@ -2079,3 +2079,47 @@ def test_sddmm_ds_ds_dd_dd():
     print(f"scorch total time: {scorch_total_time}")
     print(f"scorch eval time: {scorch_eval_time}")
     print(f"scorch eval time / torch time: {scorch_eval_time / torch_time}")
+
+
+def test_spmv_d_oo_d():
+    """
+    Compare speed of torch and scorch Sparse matrix * Dense vector
+    Use random tensors
+    """
+    # y[i] = sum_j A[i, j] * x[j]
+    # Randomly generate sparse matrix A, which is m by n
+    m = 10000
+    n = 10000
+    sparsity = 0.9
+    random_tensor_a = torch.rand(m, n)
+    random_tensor_x = torch.rand(n)
+    # Sparsify A
+    random_tensor_a = random_tensor_a * (torch.rand(m, n) > sparsity).float()
+    random_tensor_a_sparse = random_tensor_a.to_sparse_coo()
+
+    start_time = time.time()
+    torch_result = torch.matmul(random_tensor_a_sparse, random_tensor_x)
+    torch_time = time.time() - start_time
+
+    tensor_a_scorch = Tensor.from_torch(random_tensor_a, "A").to_sparse("oo")
+    tensor_x_scorch = Tensor.from_torch(random_tensor_x, "x")
+
+    time_dict = {}
+    start_time = time.time()
+    scorch_result = einsum(
+        "ij,j->i",
+        tensor_a_scorch,
+        tensor_x_scorch,
+        time_dict=time_dict,
+        format="d",
+    )
+    # scorch_result = matmul(tensor_a_scorch, tensor_x_scorch, time_dict=time_dict)
+    scorch_total_time = time.time() - start_time
+    scorch_eval_time = time_dict["eval_time"]
+
+    assert torch.allclose(torch_result, scorch_result.values)
+
+    print(f"torch time: {torch_time}")
+    print(f"scorch total time: {scorch_total_time}")
+    print(f"scorch eval time: {scorch_eval_time}")
+    print(f"scorch eval time / torch time: {scorch_eval_time / torch_time}")
