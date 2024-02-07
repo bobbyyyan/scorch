2ef2c6e9757678a5387bb52dafa7e6ed49d18646
Chris Gyurgyik <gyurgyikcp@gmail.com>
2024-02-06 19:07:00 -0800

    add ops file

---
+++ src/scorch/compiler/shapes/ops.py
@@ -0,0 +1,10 @@
+import torch
+
+from scorch.compiler import cin
+from scorch import tensor
+from scorch.compiler.shapes import cfir, codegen, cpp
+from scorch.format import LevelType
+from typing import List, Optional, Any, Tuple, Callable, Union, Sequence
+
+# TODO(cgyurgyik): Add high-level shape operators.
+# TODO(cgyurgyik): Reuse high-level linear algebra operations from Scorch-original.
