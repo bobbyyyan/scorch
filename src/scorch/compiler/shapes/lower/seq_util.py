cdbfa300e23eaf12b5dd040852e99c9e258f3d9f
Chris Gyurgyik <gyurgyikcp@gmail.com>
2024-03-08 14:00:37 -0800

    nit

--- src/scorch/compiler/shapes/lower/seq_util.py
+++ src/scorch/compiler/shapes/lower/seq_util.py
@@ -2,7 +2,7 @@ from scorch.compiler import cin
 from typing import List, Optional, Any, Tuple, Callable, Union, Sequence
 import scorch.format as format
 
-# Utility functions for sequence expressions (seq).
+# Utility functions for sequence expressions (Seq).
 
 
 def Size(sexpr: cin.Seq) -> int:
