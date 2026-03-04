d7a9cdc82819750c1a1285cded8385165ea51371
Bobby Yan <me@bobbyy.org>
2026-03-04 10:50:28 -0800

    Refactor LLIRLowerer: extract expression, loop, conditional, and function lowering into dedicated methods

--- src/scorch/compiler/codegen.py
+++ src/scorch/compiler/codegen.py
@@ -40,9 +40,6 @@ class LLIRLowerer:
         elif isinstance(ir, llir.BlankLine):
             return self.lower_llir(" ", indent_level)
 
-        elif isinstance(ir, llir.Literal):
-            return self.lower_llir(str(ir.value), indent_level)
-
         elif isinstance(ir, llir.VarInit):
             return self.lower_llir(
                 f"{ir.var.type.value} {ir.var.name} {ir.op} {self.lower_llir(ir.value)};",
@@ -61,141 +58,22 @@ class LLIRLowerer:
                 indent_level,
             )
 
-        elif isinstance(ir, llir.Cast):
-            return self.lower_llir(
-                f"({ir.data_type.value}) {self.lower_llir(ir.expr)}", indent_level
-            )
+        elif isinstance(ir, (llir.Literal, llir.Cast, llir.Sizeof, llir.BinOp,
+                             llir.UnaryOp, llir.FunctionCall, llir.Array,
+                             llir.ArrayAccess)):
+            return self.lower_expression(ir, indent_level)
 
-        elif isinstance(ir, llir.Sizeof):
-            return self.lower_llir(f"sizeof({ir.data_type.value})", indent_level)
-
-        elif isinstance(ir, llir.BinOp):
-            return self.lower_llir(
-                f"{self.lower_llir(ir.left)} {ir.op} {self.lower_llir(ir.right)}",
-                indent_level,
-            )
-        elif isinstance(ir, llir.UnaryOp):
-            return self.lower_llir(
-                f"{ir.op} {self.lower_llir(ir.operand)}", indent_level
-            )
-        elif isinstance(ir, llir.FunctionCall):
-            return self.lower_llir(
-                f"{ir.name}({', '.join([self.lower_llir(arg) for arg in ir.args])})",
-                indent_level,
-            )
         elif isinstance(ir, llir.FunctionCallStmt):
             return self.lower_llir(
                 f"{ir.name}({', '.join([self.lower_llir(arg) for arg in ir.args])});",
                 indent_level,
             )
-        elif isinstance(ir, llir.Array):
-            return self.lower_llir(
-                f"{{{', '.join([self.lower_llir(v) for v in ir.values])}}}",
-                indent_level,
-            )
 
-        elif isinstance(ir, llir.ArrayAccess):
-            return self.lower_llir(
-                f"{self.lower_llir(ir.array)}[{self.lower_llir(ir.index)}]",
-                indent_level,
-            )
-
-        elif isinstance(ir, llir.WhileLoop):
-            return (
-                self.lower_llir(f"while ({self.lower_llir(ir.cond)}) {{", indent_level)
-                + "\n"
-                + self.lower_llir(ir.body, indent_level + 1)
-                + "\n"
-                + self.lower_llir("}", indent_level)
-            )
-
-        elif isinstance(ir, llir.ForLoop):
-            init_lowered = self.lower_llir(ir.init) if ir.init is not None else ";"
-
-            return (
-                self.lower_llir(
-                    f"for ({init_lowered} {self.lower_llir(ir.cond)}; {self.lower_llir(ir.update, no_semicolon=True)}) {{",
-                    indent_level,
-                )
-                + "\n"
-                + self.lower_llir(ir.body, indent_level + 1)
-                + "\n"
-                + self.lower_llir("}", indent_level)
-            )
-
-        elif isinstance(ir, llir.ForLoopAuto):
-            # for (auto i : arr) {
-            #   ...
-            # }
-            return (
-                self.lower_llir(
-                    f"for ({ir.var.type.value} {self.lower_llir(ir.var)} : {self.lower_llir(ir.array)}) {{",
-                    indent_level,
-                )
-                + "\n"
-                + self.lower_llir(ir.body, indent_level + 1)
-                + "\n"
-                + self.lower_llir("}", indent_level)
-            )
+        elif isinstance(ir, (llir.WhileLoop, llir.ForLoop, llir.ForLoopAuto)):
+            return self.lower_loop_construct(ir, indent_level)
 
         elif isinstance(ir, llir.IfThenElse):
-            # Handle the case where cond is a list of conditions and then_body is a list of list
-            # of statements
-            result = ""
-            if ir.cond_list:
-                assert ir.then_body_list, "then_body_list must be provided"
-                assert len(ir.cond_list) == len(
-                    ir.then_body_list
-                ), "Number of conditions and then_body's must be the same"
-
-                total_num_conds = len(ir.cond_list) + (1 if ir.else_body else 0)
-
-                for i, cond in enumerate(ir.cond_list):
-                    if i == 0:
-                        result += (
-                            self.lower_llir(
-                                f"if ({self.lower_llir(cond)}) {{", indent_level
-                            )
-                            + "\n"
-                            + self.lower_llir(ir.then_body_list[i], indent_level + 1)
-                            + "\n"
-                        )
-                    elif ir.make_last_case_else and i == total_num_conds - 1:
-                        result += (
-                            self.lower_llir("} else {", indent_level)
-                            + "\n"
-                            + self.lower_llir(ir.then_body_list[i], indent_level + 1)
-                            + "\n"
-                        )
-                    else:
-                        result += (
-                            self.lower_llir(
-                                f"}} else if ({self.lower_llir(cond)}) {{", indent_level
-                            )
-                            + "\n"
-                            + self.lower_llir(ir.then_body_list[i], indent_level + 1)
-                            + "\n"
-                        )
-            else:
-                assert ir.cond, "If condition must be provided"
-                assert ir.then_body, "If then body must be provided"
-                result += (
-                    self.lower_llir(f"if ({self.lower_llir(ir.cond)}) {{", indent_level)
-                    + "\n"
-                    + self.lower_llir(ir.then_body, indent_level + 1)
-                    + "\n"
-                )
-
-            if ir.else_body:
-                result += (
-                    self.lower_llir("} else {", indent_level)
-                    + "\n"
-                    + self.lower_llir(ir.else_body, indent_level + 1)
-                    + "\n"
-                )
-
-            result += self.lower_llir("}", indent_level)
-            return result
+            return self.lower_conditional(ir, indent_level)
 
         elif isinstance(ir, llir.Var):
             return ir.name
@@ -209,25 +87,7 @@ class LLIRLowerer:
             return self.lower_llir(f"{ir.var.name}++;", indent_level)
 
         elif isinstance(ir, llir.Function):
-            # args must be llir.Var's
-            if ir.args:
-                assert [
-                    isinstance(arg, llir.Var) for arg in ir.args
-                ], "Args must be llir.Var's"
-            args = cast(List[llir.Var], ir.args)
-            # Assert all the args have types so that type checker is happy
-            assert all([arg.type for arg in args]), "All args must have types"
-            return (
-                self.lower_llir(
-                    f"{ir.return_type.value} {ir.name}"
-                    + f"({', '.join([f'{arg.type.value} {arg.name}' for arg in args])}) {{",
-                    indent_level,
-                )
-                + "\n"
-                + self.lower_llir(ir.body, indent_level + 1)
-                + "\n"
-                + self.lower_llir("}", indent_level)
-            )
+            return self.lower_function_definition(ir, indent_level)
 
         elif isinstance(ir, llir.Return):
             return self.lower_llir(f"return {self.lower_llir(ir.value)};", indent_level)
@@ -235,4 +95,161 @@ class LLIRLowerer:
         return self.lower_llir(
             f"No code gen implemented for node type: {ir.__class__.__name__}",
             indent_level,
+        )
+
+    def lower_expression(
+        self,
+        ir: Union[llir.Literal, llir.Cast, llir.Sizeof, llir.BinOp,
+                  llir.UnaryOp, llir.FunctionCall, llir.Array, llir.ArrayAccess],
+        indent_level: int = 0,
+    ) -> str:
+        if isinstance(ir, llir.Literal):
+            return self.lower_llir(str(ir.value), indent_level)
+
+        elif isinstance(ir, llir.Cast):
+            return self.lower_llir(
+                f"({ir.data_type.value}) {self.lower_llir(ir.expr)}", indent_level
+            )
+
+        elif isinstance(ir, llir.Sizeof):
+            return self.lower_llir(f"sizeof({ir.data_type.value})", indent_level)
+
+        elif isinstance(ir, llir.BinOp):
+            return self.lower_llir(
+                f"{self.lower_llir(ir.left)} {ir.op} {self.lower_llir(ir.right)}",
+                indent_level,
+            )
+
+        elif isinstance(ir, llir.UnaryOp):
+            return self.lower_llir(
+                f"{ir.op} {self.lower_llir(ir.operand)}", indent_level
+            )
+
+        elif isinstance(ir, llir.FunctionCall):
+            return self.lower_llir(
+                f"{ir.name}({', '.join([self.lower_llir(arg) for arg in ir.args])})",
+                indent_level,
+            )
+
+        elif isinstance(ir, llir.Array):
+            return self.lower_llir(
+                f"{{{', '.join([self.lower_llir(v) for v in ir.values])}}}",
+                indent_level,
+            )
+
+        elif isinstance(ir, llir.ArrayAccess):
+            return self.lower_llir(
+                f"{self.lower_llir(ir.array)}[{self.lower_llir(ir.index)}]",
+                indent_level,
+            )
+
+        raise ValueError(f"Unknown expression type: {type(ir)}")
+
+    def lower_loop_construct(
+        self,
+        ir: Union[llir.WhileLoop, llir.ForLoop, llir.ForLoopAuto],
+        indent_level: int = 0,
+    ) -> str:
+        if isinstance(ir, llir.WhileLoop):
+            header = f"while ({self.lower_llir(ir.cond)}) {{"
+        elif isinstance(ir, llir.ForLoop):
+            init_lowered = self.lower_llir(ir.init) if ir.init is not None else ";"
+            header = (
+                f"for ({init_lowered} {self.lower_llir(ir.cond)};"
+                f" {self.lower_llir(ir.update, no_semicolon=True)}) {{"
+            )
+        elif isinstance(ir, llir.ForLoopAuto):
+            header = (
+                f"for ({ir.var.type.value} {self.lower_llir(ir.var)}"
+                f" : {self.lower_llir(ir.array)}) {{"
+            )
+        else:
+            raise ValueError(f"Unknown loop type: {type(ir)}")
+
+        return (
+            self.lower_llir(header, indent_level)
+            + "\n"
+            + self.lower_llir(ir.body, indent_level + 1)
+            + "\n"
+            + self.lower_llir("}", indent_level)
+        )
+
+    def lower_conditional(
+        self, ir: llir.IfThenElse, indent_level: int = 0
+    ) -> str:
+        result = ""
+        if ir.cond_list:
+            assert ir.then_body_list, "then_body_list must be provided"
+            assert len(ir.cond_list) == len(
+                ir.then_body_list
+            ), "Number of conditions and then_body's must be the same"
+
+            total_num_conds = len(ir.cond_list) + (1 if ir.else_body else 0)
+
+            for i, cond in enumerate(ir.cond_list):
+                if i == 0:
+                    result += (
+                        self.lower_llir(
+                            f"if ({self.lower_llir(cond)}) {{", indent_level
+                        )
+                        + "\n"
+                        + self.lower_llir(ir.then_body_list[i], indent_level + 1)
+                        + "\n"
+                    )
+                elif ir.make_last_case_else and i == total_num_conds - 1:
+                    result += (
+                        self.lower_llir("} else {", indent_level)
+                        + "\n"
+                        + self.lower_llir(ir.then_body_list[i], indent_level + 1)
+                        + "\n"
+                    )
+                else:
+                    result += (
+                        self.lower_llir(
+                            f"}} else if ({self.lower_llir(cond)}) {{", indent_level
+                        )
+                        + "\n"
+                        + self.lower_llir(ir.then_body_list[i], indent_level + 1)
+                        + "\n"
+                    )
+        else:
+            assert ir.cond, "If condition must be provided"
+            assert ir.then_body, "If then body must be provided"
+            result += (
+                self.lower_llir(f"if ({self.lower_llir(ir.cond)}) {{", indent_level)
+                + "\n"
+                + self.lower_llir(ir.then_body, indent_level + 1)
+                + "\n"
+            )
+
+        if ir.else_body:
+            result += (
+                self.lower_llir("} else {", indent_level)
+                + "\n"
+                + self.lower_llir(ir.else_body, indent_level + 1)
+                + "\n"
+            )
+
+        result += self.lower_llir("}", indent_level)
+        return result
+
+    def lower_function_definition(
+        self, ir: llir.Function, indent_level: int = 0
+    ) -> str:
+        if ir.args:
+            assert [
+                isinstance(arg, llir.Var) for arg in ir.args
+            ], "Args must be llir.Var's"
+        args = cast(List[llir.Var], ir.args)
+        assert all([arg.type for arg in args]), "All args must have types"
+        header = (
+            f"{ir.return_type.value} {ir.name}"
+            + f"({', '.join([f'{arg.type.value} {arg.name}' for arg in args])}) {{"
+        )
+        return (
+            self.lower_llir(header, indent_level)
+            + "\n"
+            + self.lower_llir(ir.body, indent_level + 1)
+            + "\n"
+            + self.lower_llir("}", indent_level)
         )
